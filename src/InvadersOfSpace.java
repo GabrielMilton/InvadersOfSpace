313d198946a56ddf858e8f1e11fe117487ed58be
GabrielMilton <gabriel_galva-diez28@milton.edu>
2026-03-31 11:25:12 -0400

    Starded to wrok on it

---
+++ src/InvadersOfSpace.java
@@ -0,0 +1,382 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferStrategy;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferStrategy;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+public class InvadersOfSpace {
+    //Basic Game Application
+//Version 2
+// Basic Object, Image, Movement
+// Astronaut moves to the right.
+// Threaded
+
+//K. Chun 8/2018
+
+//*******************************************************************************
+//Import Section
+//Add Java libraries needed for the game
+//import java.awt.Canvas;
+
+//Graphics Libraries
+
+
+
+    //*******************************************************************************
+// Class Definition Section
+// step 1: implent keylisitener
+// step 1" omplent Mouslisinter
+    public class InvadersOfSpace implements Runnable, KeyListener, MouseListener {
+
+        //Variable Definition Section
+        //Declare the variables used in the program
+        //You can set their initial values too
+
+        //Sets the width and height of the program window
+        final int WIDTH = 1000;
+        final int HEIGHT = 700;
+
+        //Declare the variables needed for the graphics
+        public JFrame frame;
+        public Canvas canvas;
+        public JPanel panel;
+
+        public BufferStrategy bufferStrategy;
+        public Image Spacedude;
+        public Image Ealien;
+        public Image Blast;
+        ;
+        public Image BackgroundPic;
+
+        //public Niastroid[] roids;
+
+        //Declare the objects used in the program
+        //These are things that are made up of more than one variable type
+        private Spacedude Hship;
+        private Ealien CsBros;
+
+
+
+        // Main method definition
+        // This is the code that runs first and automatically
+        public static void main(String[] args) {
+            InvadersOfSpace ex = new InvadersOfSpace();   //creates a new instance of the game
+            new Thread(ex).start();                 //creates a threads & starts up the code in the run( ) method
+        }
+
+
+        // Constructor Method
+        // This has the same name as the class
+        // This section is the setup portion of the program
+        // Initialize your variables and construct your program objects here.
+        public InvadersOfSpace() {
+
+            setUpGraphics();
+            //random strucutre
+            //(int) (Math. Random() * range) + start(where the range starts)
+            //range is 1-10
+            int randx = (int) (Math.random() * 10) + 1;
+            //range 1-999
+            randx = (int) (Math.random() * 999) + 1;
+
+            int randy = (int) (Math.random() * 699) + 1;
+            int randpx;
+            int randpy;
+            int randtx;
+            int randty;
+            randpx = (int) (Math.random() * 999) + 1;
+            randpy = (int) (Math.random() * 699) + 1;
+            randtx = (int) (Math.random() * 999) + 1;
+            randty = (int) (Math.random() * 699) + 1;
+
+
+            //todo make a varible rand y that generates a random number between 1-699
+
+            //variable and objects
+            //create (construct) the objects needed for the game and load up
+            Spacedude = Toolkit.getDefaultToolkit().getImage("SpaceBus.jpeg"); //load the picture
+            Blast = Toolkit.getDefaultToolkit().getImage("GDAWG.png");
+            BackgroundPic = Toolkit.getDefaultToolkit().getImage("TheMichealsun.jpg");
+
+            Hship = new Spacedude(randx, randy);
+            CsBros = new Ealien(randx, randy);
+
+            //Niamthemenece.dx = -Niamthemenece.dx; - use this to change the dx or dy of two objects that come from the same class
+
+
+        }// InvadersOfSpace()
+
+
+//*******************************************************************************
+//User Method Section
+//
+// put your code to do things here.
+
+        // main thread
+        // this is the code that plays the game after you set things up
+        public void run() {
+
+            //for the moment we will loop things forever.
+            while (true) {
+
+                moveThings();  //move all the game objects
+                render();  // paint the graphics
+                pause(20); // sleep for 10 ms
+            }
+        }
+
+
+        public void moveThings() {
+            //calls the move( ) code in the objects
+            Hship.move();
+            CsBros.move();
+
+
+
+            for (int m = 0; m < roids.length; m = m + 1) {
+                roids[m].move();
+
+            }
+
+
+        }
+
+       /*/ public void crashing() {
+            // check to see if my astros crash into eachother
+            if (astro.hitBox.intersects(Niamlikespickles.hitBox)) {
+                System.out.println("CRASH!!!");
+                astro.dx = -astro.dx;
+                Niamlikespickles.dx = -Niamlikespickles.dx;
+                astro.dy = -astro.dy;
+                Niamlikespickles.dy = -Niamlikespickles.dy;
+
+
+            }
+            if (Niamthemenece.hitBox.intersects(astro.hitBox) && Niamthemenece.iscrasinhg == false) {
+                System.out.println("BOOM!!!");
+                Niamthemenece.height += astro.height;
+                Niamthemenece.width += astro.width;
+                Niamthemenece.iscrasinhg = true;
+                astro.isAlive = false;
+
+
+
+
+
+           Niamthemenece.dx = -Niamthemenece.dx;
+            Niamthemenece.dx = -Tobyterror.dx;
+            Niamthemenece.dy = -Niamthemenece.dy;
+            Niamthemenece.dy = -Tobyterror.dy;
+
+
+
+            }
+// ! = saying we want the oppiste thing to happen
+            if (!Niamthemenece.hitBox.intersects(astro.hitBox)) {
+                astro.iscrasinhg = false;
+
+            }
+
+            if (!Tobyterror.hitBox.intersects(Niamlikespickles.hitBox)) {
+                Niamlikespickles.iscrasinhg = false;
+
+            }
+            if (Tobyterror.hitBox.intersects(Niamlikespickles.hitBox) && Niamlikespickles.iscrasinhg == false) {
+                System.out.println("BOOM!!!");
+                Tobyterror.height += 50;
+                Niamlikespickles.iscrasinhg = true;
+                Niamlikespickles.isAlive = false;
+
+
+
+
+
+          Niamthemenece.dx = -Niamthemenece.dx;
+            Niamthemenece.dx = -Tobyterror.dx;
+            Niamthemenece.dy = -Niamthemenece.dy;
+            Niamthemenece.dy = -Tobyterror.dy;
+
+
+                for (int c = 0; c < roids.length; c = c + 1) {
+                    if (roids[c].hitBox.intersects(astro.hitBox) && astro.iscrasinhg == false) {
+                        System.out.println("astroid crash");
+                    }
+                }
+
+
+            }
+
+
+        }
+
+        //Pauses or sleeps the computer for the amount specified in milliseconds
+        public void pause(int time) {
+            //sleep
+            try {
+                Thread.sleep(time);
+            } catch (InterruptedException e) {
+
+            }
+        }
+
+        //Graphics setup method
+        private void setUpGraphics() {
+            frame = new JFrame("Application Template");   //Create the program window or frame.  Names it.
+
+            panel = (JPanel) frame.getContentPane();  //sets up a JPanel which is what goes in the frame
+            panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));  //sizes the JPanel
+            panel.setLayout(null);   //set the layout
+
+            // creates a canvas which is a blank rectangular area of the screen onto which the application can draw
+            // and trap input events (Mouse and Keyboard events)
+            canvas = new Canvas();
+            canvas.setBounds(0, 0, WIDTH, HEIGHT);
+            canvas.setIgnoreRepaint(true);
+
+            //step 2: as canvas as the keylisten
+            canvas.addKeyListener(this);
+            //step: 2 set canvas
+            canvas.addMouseListener(this);
+            panel.add(canvas);  // adds the canvas to the panel.
+
+            // frame operations
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //makes the frame close and exit nicely
+            frame.pack();  //adjusts the frame and its contents so the sizes are at their default or larger
+            frame.setResizable(false);   //makes it so the frame cannot be resized
+            frame.setVisible(true);      //IMPORTANT!!!  if the frame is not set to visible it will not appear on the screen!
+
+            // sets up things so the screen displays images nicely.
+            canvas.createBufferStrategy(2);
+            bufferStrategy = canvas.getBufferStrategy();
+            canvas.requestFocus();
+            System.out.println("DONE graphic setup");
+
+        }
+
+
+        //paints things on the screen using bufferStrategy
+        private void render() {
+            Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+            g.clearRect(0, 0, WIDTH, HEIGHT);
+            // Start adding things here
+            g.drawImage(BackgroundPic, 0, 0, WIDTH, HEIGHT, null);
+
+            // When the niamastroid intercects with gdawg, GDAWG disapears
+            if (Hship.isAlive == true) {
+                g.drawImage(Spacedude, Hship.xpos, Hship.ypos, Hship.width, Hship.height, null);
+                g.drawRect(Hship.hitBox.x, Hship.hitBox.y, Hship.hitBox.width, Hship.hitBox.height);
+
+            }
+            /*?
+            if (Niamlikespickles.isAlive == true) {
+                g.drawImage(tired, Niamlikespickles.xpos, Niamlikespickles.ypos, Niamlikespickles.width, Niamlikespickles.height, null);
+
+            }
+            for(int r=0; r<roids.length; r=r+1) {
+                g.drawImage(Niastroid,roids[r].xpos,roids[r].ypos,roids[r].height,roids[r].width,null);
+            }
+            g.drawImage(Niastroid, Niamthemenece.xpos, Niamthemenece.ypos, Niamthemenece.width, Niamthemenece.height, null);
+            g.drawImage(Tobyroid, Tobyterror.xpos, Tobyterror.ypos, Tobyterror.width, Tobyterror.height, null);
+            // use this to draw hitbox
+
+// end of adding things
+            g.dispose();
+
+            bufferStrategy.show();
+        }
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            System.out.println("key typed " + e.getKeyCode());
+            // up arrow is 38
+            if (e.getKeyCode()== 38){
+                System.out.println("pressed up arrow");
+                Niamthemenece.ypos = Niamthemenece.ypos - 5;
+                Niamthemenece.dy = -Math.abs(Niamthemenece.dy);
+            }
+
+            if (e.getKeyCode()== 40){
+                System.out.println("pressed down arrow");
+                Niamthemenece.ypos = Niamthemenece.ypos + 5;
+                Niamthemenece.dy = Math.abs(Niamthemenece.dy);
+            }
+            if (e.getKeyCode()== 39){
+                System.out.println("pressed right arrow");
+                Niamthemenece.xpos = Niamthemenece.xpos + 5;
+                Niamthemenece.dx = Math.abs(Niamthemenece.dx);
+
+
+
+            }
+            if (e.getKeyCode()== 37){
+                System.out.println("pressed left arrow");
+                Niamthemenece.xpos = Niamthemenece.xpos -5;
+                Niamthemenece.dx = -Math.abs(Niamthemenece.dx);
+            }
+        }
+
+        //Step3: added
+        @Override
+        public void keyReleased(KeyEvent e) {
+
+            if (e.getKeyCode() == 38) {
+                System.out.println("not pressed up arrow");
+                Niamthemenece.dy = 0;
+            }
+
+            if (e.getKeyCode() == 40) {
+                System.out.println("not pressed up arrow");
+                Niamthemenece.dy = 0;
+            }
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            astro.xpos = e.getX();
+            astro.ypos = e.getY();
+            System.out.println(e.getClickCount());
+            if(e.getClickCount()==2){
+                astro.width = astro.width*2;
+                astro.height = astro.height*2;
+            }
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            System.out.println("enterred!!");
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+        }
+        //step 3 add keylisitener method
+    }
+    /*/
+        }
+    }
+}
